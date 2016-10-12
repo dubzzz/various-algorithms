cc5d1d08ae444bafa85c9c282da906fa4aec88c7
Nicolas DUBIEN <github@dubien.org>
2016-10-12 21:38:04 +0200

    [arrays/all_unique] Revamp no-data-structure solution to work with custom helper (long long in the code)

--- cracking-the-coding/1-array-string/1.1-all-unique/main.cpp
+++ cracking-the-coding/1-array-string/1.1-all-unique/main.cpp
@@ -27,12 +27,18 @@ bool all_unique_struct(std::string const& in)
 
 bool all_unique_nostruct(std::string const& in)
 {
-  unsigned char unicity_checker[32] = { 0 }; //8 bits x 32 = 256bits
+  typedef long long HelperType;
+  const std::size_t __possible_elements = 1 << (sizeof(char) << 3);
+  const std::size_t __elements_per_helper = sizeof(HelperType) << 3;
+  static_assert(__possible_elements % __elements_per_helper == 0, "8 x sizeof(HelperType) must divide 8 x sizeof(char)");
+
+  HelperType unicity_checker[__possible_elements / __elements_per_helper] = { 0 }; //should be a ceil
+  
   for (auto c : in)
   {
     const unsigned char cc = *reinterpret_cast<unsigned char*>(&c);
-    const unsigned int idx = static_cast<unsigned int>(cc) >> 3;
-    const unsigned char mask = 1 << (static_cast<unsigned int>(cc) & 0b0111);
+    const std::size_t idx = cc / __elements_per_helper;
+    const HelperType mask = static_cast<HelperType>(1) << (cc & (__elements_per_helper-1)); // << (cc & __elements_per_helper)
     if (unicity_checker[idx] & mask)
     {
       return false;
