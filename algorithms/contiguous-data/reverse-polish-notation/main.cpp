c21015d217cf1e8dd9197f800120a25fa9fb06a7
Nicolas DUBIEN <github@dubien.org>
2016-11-23 08:59:18 +0100

    [contiguous-data/reverse-polish-notation] Add data Expression structure for RapidCheck tests

--- algorithms/contiguous-data/reverse-polish-notation/main.cpp
+++ algorithms/contiguous-data/reverse-polish-notation/main.cpp
@@ -96,6 +96,96 @@ TEST(ALGO, BasicExample2)
   ASSERT_EQ(6, eval(std::begin(vs), std::end(vs)));
 }
 
+class Expression
+{
+public:
+  virtual std::unique_ptr<Expression> clone() const = 0;
+  virtual long long eval() const = 0; // evaluate the expression
+  virtual std::vector<std::string> polish() const = 0; // construct a polish interpretation of the expression
+};
+
+class Number final : public Expression
+{
+  const short _value;
+
+public:
+  explicit Number(short value) : Expression(), _value(value) {}
+  Number(Number const&) = default;
+  Number(Number&&) = default;
+  Number& operator=(Number const&) = delete;
+  Number& operator=(Number&&) = delete;
+  
+  std::unique_ptr<Expression> clone() const override { return std::make_unique<Number>(*this); }
+  long long eval() const override { return _value; }
+  std::vector<std::string> polish() const override { return std::vector<std::string>{ std::to_string(_value) }; }
+};
+
+struct Plus
+{
+  static constexpr char symbol() { return '+'; }
+  static long long eval(long long a, long long b) { return a+b; }
+};
+struct Minus
+{
+  static constexpr char symbol() { return '-'; }
+  static long long eval(long long a, long long b) { return a-b; }
+};
+struct Multiply
+{
+  static constexpr char symbol() { return '*'; }
+  static long long eval(long long a, long long b) { return a*b; }
+};
+struct Divide
+{
+  static constexpr char symbol() { return '/'; }
+  static long long eval(long long a, long long b) { return b != 0L ? a/b : 0L; }
+};
+
+template <class Operation> class BinaryOperation final : public Expression
+{
+  std::unique_ptr<Expression> _left;
+  std::unique_ptr<Expression> _right;
+
+public:
+  BinaryOperation(std::unique_ptr<Expression>&& left, std::unique_ptr<Expression>&& right) : Expression(), _left(left), _right(right)
+  {
+    assert(_left);
+    assert(_right);
+  }
+  
+  BinaryOperation(BinaryOperation<Operation> const& other) : Expression(), _left(other._left->clone()), _right(other._right->clone()) {}
+  BinaryOperation(BinaryOperation<Operation>&&) = default;
+  BinaryOperation& operator=(BinaryOperation<Operation> const&) = delete;
+  BinaryOperation& operator=(BinaryOperation<Operation>&&) = delete;
+  
+  std::unique_ptr<Expression> clone() const override { return std::make_unique<BinaryOperation<Operation>>(_left->clone(), _right->clone()); }
+  long long eval() const override { return Operation::eval(_left->eval(), _right->eval()); }
+  std::vector<std::string> polish() const override
+  {
+    auto ret = std::vector<std::string>{ _left->polish() };
+    auto&& right_ret { _right->polish() };
+    std::copy(std::begin(right_ret), std::end(right_ret), std::back_inserter(ret));
+    ret.emplace_back(Operation::symbol());
+    return ret;
+  }
+};
+
+RC_GTEST_PROP(ALGO, RandomValidExpression, ())
+{
+  /*std::string s2 = *rc::gen::apply([s](unsigned seed) {
+    std::vector<char> remap(UCHAR_MAX +1);
+    std::iota(std::begin(remap), std::end(remap), CHAR_MIN);
+    std::mt19937 g(seed);
+    std::shuffle(std::begin(remap), std::end(remap), g);
+    
+    std::string s2;
+    std::transform(std::begin(s), std::end(s), std::back_inserter(s2), [&remap](char cur) { return remap[static_cast<int>(cur) - CHAR_MIN]; });
+    return s2;
+  }, rc::gen::arbitrary<unsigned>()).as("isomorphic transformation of s (s2)");
+*/
+  RC_ASSERT(true);
+}
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
