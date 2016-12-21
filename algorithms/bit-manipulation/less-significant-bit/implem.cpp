e5f192d9dc4654e97045cdc80dbfe0dc2a432572
Nicolas DUBIEN <github@dubien.org>
2016-12-22 00:46:52 +0100

    [bit-manipulation/less-significant-bit] Unit-tests and code

---
+++ algorithms/bit-manipulation/less-significant-bit/implem.cpp
@@ -0,0 +1,15 @@
+// Algorithm to be tested
+
+unsigned most_significant(unsigned num)
+{
+  for ( ; (num & (num -1)) != 0 ; num &= num -1) {}
+  return num;
+}
+
+unsigned less_significant(unsigned num)
+{
+  return num == 0 ? 0 : most_significant(num ^ (num -1));
+}
+
+#include "tests.hpp"
+
