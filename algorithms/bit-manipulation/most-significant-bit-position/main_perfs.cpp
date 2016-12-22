804408b1971d41d68fa973d2f7fcfb9899df5edf
Nicolas DUBIEN <github@dubien.org>
2016-12-22 22:46:01 +0100

    [cmake] Multiple main possibles with new CMakeLists.txt

--- algorithms/bit-manipulation/most-significant-bit-position/main_perfs.cpp
+++ algorithms/bit-manipulation/most-significant-bit-position/main_perfs.cpp
@@ -1,14 +1,16 @@
+//#include <iostream>
 unsigned most_significant_position(unsigned);
 
 int main(int argc, char **argv)
 {
   unsigned out {};
-  constexpr unsigned num_runs = 1e8;
+  constexpr unsigned num_runs = 1e7;
   for (unsigned run_id {} ; run_id != num_runs ; ++run_id)
   {
     out += most_significant_position(run_id);
   }
-  bool success = out == unsigned(5);
-  return out;
+  //std::cout << out;
+  bool success = out == unsigned(223222785);
+  return success;
 }
 
