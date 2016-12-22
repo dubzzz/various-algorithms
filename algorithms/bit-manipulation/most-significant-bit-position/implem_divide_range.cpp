d19f82110b3634fac08be101a993b04576ce5dd3
Nicolas DUBIEN <github@dubien.org>
2016-12-22 23:02:14 +0100

    [clang] Bad version detection of clang

--- algorithms/bit-manipulation/most-significant-bit-position/implem_divide_range.cpp
+++ algorithms/bit-manipulation/most-significant-bit-position/implem_divide_range.cpp
@@ -1,5 +1,5 @@
 #if (defined(__clang__) && (__clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ < 4))) || \
-    (defined(__GNUC__)  && (__GNUC__        < 5 || (__GNUC__        == 5 && __GNUC_MINOR__  < 1)))
+    (! defined(__clang__) && defined(__GNUC__)  && (__GNUC__        < 5 || (__GNUC__        == 5 && __GNUC_MINOR__  < 1)))
 #   define NO_CONSTEXPR
 #endif
 
