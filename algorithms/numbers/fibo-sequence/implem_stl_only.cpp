de3313b9000e932ac758bd3de0cbb06db79494bd
Nicolas DUBIEN <github@dubien.org>
2016-12-22 23:31:16 +0100

    [numbers/fibo-sequence] Unit-tests and code

---
+++ algorithms/numbers/fibo-sequence/implem_stl_only.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <vector>
+
+// Algorithm to be tested
+
+std::vector<unsigned long long> build_fibos(unsigned num)
+{
+  if (num == 0)
+  {
+    return {};
+  }
+  std::vector<unsigned long long> vs(num, 1);
+  std::adjacent_difference(vs.begin(), vs.end()-1, vs.begin()+1, std::plus<unsigned long long>());
+  return vs;
+}
+
+#include "tests.hpp"
+
