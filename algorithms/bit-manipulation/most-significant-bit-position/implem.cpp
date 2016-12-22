d5444743373de09ab8af30c4bae8892c5a360dd9
Nicolas DUBIEN <github@dubien.org>
2016-12-22 01:18:40 +0100

    [bit-manipulation/most-significant-bit-position] Unit-tests and code

---
+++ algorithms/bit-manipulation/most-significant-bit-position/implem.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <utility>
+
+// Algorithm to be tested
+
+template <std::size_t I> constexpr unsigned pow_of_2()
+{
+  return unsigned(1) << I;
+}
+
+template <std::size_t... I> unsigned most_significant_position_helper(unsigned num, std::index_sequence<I...>)
+{
+  constexpr unsigned values[] = { pow_of_2<I>()... };
+  for (std::size_t idx {} ; idx != sizeof...(I) ; ++idx)
+  {
+    if ((num ^ values[idx]) < values[idx])
+    {
+      return idx +1;
+    }
+  }
+  return 0;
+}
+
+unsigned most_significant_position(unsigned num)
+{
+  using defining_sequence = std::make_index_sequence<8 * sizeof(unsigned)>;
+  return most_significant_position_helper(num, defining_sequence());
+}
+
+#include "tests.hpp"
+
---
+++ algorithms/bit-manipulation/most-significant-bit-position/tests.hpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include <rapidcheck/gtest.h>
+
+// Compute the most significant bit position of an unsigned
+// eg.: most significant bit of 9 (0b1001) is 4th bit so answer is 4
+
+// Algorithm to be tested
+// unsigned most_significant_position(unsigned)
+
+// Running tests
+
+TEST(TEST_NAME, Zero)
+{
+  ASSERT_EQ(0, most_significant_position(0));
+}
+
+TEST(TEST_NAME, PowerOfTwo)
+{
+  ASSERT_EQ(8, most_significant_position(128));
+}
+
+TEST(TEST_NAME, Nine)
+{
+  ASSERT_EQ(4, most_significant_position(9));
+}
+
+TEST(TEST_NAME, LargeNumber)
+{
+  ASSERT_EQ(32, most_significant_position(2147483648));
+}
+
+RC_GTEST_PROP(TEST_NAME, AnswerIsNotNullForNotNull, (unsigned N))
+{
+  RC_PRE(N > unsigned());
+  RC_ASSERT(most_significant_position(N) > unsigned());
+}
+
+RC_GTEST_PROP(TEST_NAME, AnswerIsBitOfInput, (unsigned N))
+{
+  RC_PRE(N > unsigned());
+  auto answer_pos = most_significant_position(N);
+  auto answer = 1 << (answer_pos -1);
+  RC_ASSERT((N | answer) == N);
+}
+
+RC_GTEST_PROP(TEST_NAME, AnswerIsTheHighestBit, (unsigned N))
+{
+  RC_PRE(N > unsigned());
+  auto answer_pos = most_significant_position(N);
+  auto answer = unsigned(1) << (answer_pos -1);
+  RC_ASSERT((N ^ answer) < answer);
+}
+
