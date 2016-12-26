6cdc90f8242a138080d013836946760bdc7fdf18
Nicolas DUBIEN <github@dubien.org>
2016-12-26 17:37:57 +0100

    [visual studio] Fix issue with most-significant-bit-position:divide_range

--- algorithms/bit-manipulation/most-significant-bit-position/implem_divide_range.cpp
+++ algorithms/bit-manipulation/most-significant-bit-position/implem_divide_range.cpp
@@ -9,19 +9,16 @@
 
 // Algorithm to be tested
 
-template <class NumType>
-constexpr NumType pow2(std::size_t i) { return NumType(1) << i; }
-
-template <class NumType, std::size_t... I>
-__CONSTEXPR__ NumType pows2(std::index_sequence<I...>)
+template <class NumType> constexpr NumType pow2(std::size_t i)
 {
-  __CONSTEXPR__ NumType data[] = { pow2<NumType>(I)... };
-  NumType agg {};
-  for (std::size_t idx {} ; idx != sizeof...(I) ; ++idx)
-  {
-    agg |= data[idx];
-  }
-  return agg;
+  return NumType(1) << i;
+}
+
+template <class NumType> constexpr NumType pows2(std::size_t num_ones)
+{
+  return num_ones == std::size_t()
+      ? NumType{}
+      : pow2<NumType>(num_ones -1) | pows2<NumType>(num_ones -1);
 }
 
 template <unsigned ones, unsigned pad, class NumType>
@@ -29,7 +26,7 @@ __CONSTEXPR__ unsigned most_significant_position_helper(NumType num)
 {
   if (! ones) return 1 + pad;
   constexpr unsigned midones = ones >> 1;
-  __CONSTEXPR__ NumType mask = pows2<NumType>(std::make_index_sequence<ones+pad>());
+  constexpr NumType mask = pows2<NumType>(ones+pad);
   return (num & ~mask)
       ? most_significant_position_helper<midones, pad + ones>(num)
       : most_significant_position_helper<midones, pad>(num);
@@ -43,7 +40,7 @@ __CONSTEXPR__ unsigned most_significant_position(unsigned num)
 #include "tests.hpp"
 
 #if defined(NO_SUPPORT_CONSTEXPR_CXX14)
-#   pragma ("warning - constexpr tests have been disabled for this compiler")
-#   pragma ("warning - requirement: compiler compatible with C++14's constexpr")
+#   pragma message("warning - constexpr tests have been disabled for this compiler")
+#   pragma message("warning - requirement: compiler compatible with C++14's constexpr")
 #endif
 
