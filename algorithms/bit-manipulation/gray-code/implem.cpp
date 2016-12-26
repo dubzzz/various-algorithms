50dbdccad81f3af1fdcbc2b5b5c4014f6bf4551f
Nicolas DUBIEN <github@dubien.org>
2016-12-26 19:00:46 +0100

    [visual studio] gray-code/implem seems to be due to a compiler bug

--- algorithms/bit-manipulation/gray-code/implem.cpp
+++ algorithms/bit-manipulation/gray-code/implem.cpp
@@ -17,15 +17,32 @@ unsigned corresponding_pow(unsigned num)
 
 std::vector<unsigned> build_grays(unsigned num)
 {
-  std::vector<unsigned> codes(2 * corresponding_pow(num)); //initial vector contains one entry
-  // Need to preallocate necessary memory in order to prevent from:
-  // If the new size() is greater than capacity() then all iterators and references (including the past-the-end iterator) are invalidated. Otherwise only the past-the-end iterator is invalidated.
+  #ifdef _MSC_VER
+    // It seems that there is a bug VS 14.0 2015
+    // Following code crashes when usin this compiler
+    //  std::vector<int> make_it_crash(1);
+    //  make_it_crash.reserve(2);
+    //  std::copy(std::rbegin(make_it_crash), std::rend(make_it_crash), std::back_inserter(make_it_crash));
+    // It seems that push_back invalidate reverse_iterator even if the capacity is right
+    std::vector<unsigned> codes(2 * corresponding_pow(num));
+  #else
+    std::vector<unsigned> codes(1); //initial vector contains one entry
+    code.reserve(2 * corresponding_pow(num));
+    // Need to preallocate necessary memory in order to prevent from:
+    // If the new size() is greater than capacity() then all iterators and references (including the past-the-end iterator) are invalidated. Otherwise only the past-the-end iterator is invalidated.
+  #endif
   
   unsigned more = 1;
   while (more < num)
   {
-    std::transform(std::next(codes.rbegin(), codes.size() - more), codes.rend()
-        , std::next(codes.begin(), more)
+    std::transform(
+        #ifdef _MSC_VER
+          std::next(codes.rbegin(), codes.size() - more), codes.rend()
+          , std::next(codes.begin(), more)
+        #else
+          codes.rbegin(), codes.rend()
+          , std::back_inserter(codes)
+        #endif
         , [more](auto prev) {
             #ifdef DEBUG
               std::cout << std::bitset<8>(prev) << " + " << std::bitset<8>(more) << std::endl;
