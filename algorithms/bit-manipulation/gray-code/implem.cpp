1802548c7338a495329830ad1a6e5a8803ed98ec
Nicolas DUBIEN <github@dubien.org>
2017-01-08 11:00:06 +0100

    [bit-manipulation/gray-code] Bug fix, past-the-end iterators invalidated by push_back

--- algorithms/bit-manipulation/gray-code/implem.cpp
+++ algorithms/bit-manipulation/gray-code/implem.cpp
@@ -15,34 +15,35 @@ unsigned corresponding_pow(unsigned num)
   return num;
 }
 
+std::reverse_iterator<const unsigned*> fixed_rbegin(std::vector<unsigned> const& vs) { return std::reverse_iterator<const unsigned*>{ vs.data() + vs.size() }; }
+std::reverse_iterator<const unsigned*> fixed_rend(std::vector<unsigned> const& vs) { return std::reverse_iterator<const unsigned*>{ vs.data() }; }
+
 std::vector<unsigned> build_grays(unsigned num)
 {
-  #ifdef _MSC_VER
-    // It seems that there is a bug VS 14.0 2015
-    // Following code crashes when usin this compiler
-    //  std::vector<int> make_it_crash(1);
-    //  make_it_crash.reserve(2);
-    //  std::copy(std::rbegin(make_it_crash), std::rend(make_it_crash), std::back_inserter(make_it_crash));
-    // It seems that push_back invalidate reverse_iterator even if the capacity is right
-    std::vector<unsigned> codes(2 * corresponding_pow(num));
-  #else
-    std::vector<unsigned> codes(1); //initial vector contains one entry
-    code.reserve(2 * corresponding_pow(num));
-    // Need to preallocate necessary memory in order to prevent from:
-    // If the new size() is greater than capacity() then all iterators and references (including the past-the-end iterator) are invalidated. Otherwise only the past-the-end iterator is invalidated.
-  #endif
+  std::vector<unsigned> codes(1); //initial vector contains one entry
+  // We cannot work directly on std::vector<unsigned>::iterator because they will be invalidated whatever the situation
+  // As stated in cppreference:
+  // <<
+  //   If the new size() is greater than capacity()
+  //   then all iterators and references (including the past-the-end iterator) are invalidated.
+  // >>
+  // One can think of reserving the necessary size in order to falsify the condition size() > capacity()
+  // by doing code.reserve(2 * corresponding_pow(num));
+  //
+  // Unfortunately, in this case the reference states that:
+  // <<
+  //   Otherwise only the past-the-end iterator is invalidated.
+  // >>
+  // Meaning that code.end() and code.rend() might be invalidated after a call to code.push_back
+  // In order to prevent that we can use indexes (positions) instead of iterators on vectors
+  // by pre-reserving the required size in order to prevent changes of pointers
+  codes.reserve(2 * corresponding_pow(num));
   
   unsigned more = 1;
   while (more < num)
   {
-    std::transform(
-        #ifdef _MSC_VER
-          std::next(codes.rbegin(), codes.size() - more), codes.rend()
-          , std::next(codes.begin(), more)
-        #else
-          codes.rbegin(), codes.rend()
-          , std::back_inserter(codes)
-        #endif
+    std::transform(fixed_rbegin(codes), fixed_rend(codes)
+        , std::back_inserter(codes)
         , [more](auto prev) {
             #ifdef DEBUG
               std::cout << std::bitset<8>(prev) << " + " << std::bitset<8>(more) << std::endl;
