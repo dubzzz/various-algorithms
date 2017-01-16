a0fba3c7aec6898eb26cf5870e9c19d8d6219118
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-01-16 23:16:01 +0100

    [most-significant-bit-position][vs] Fix compile double to unsigned

--- algorithms/bit-manipulation/most-significant-bit-position/main_perfs.cpp
+++ algorithms/bit-manipulation/most-significant-bit-position/main_perfs.cpp
@@ -4,7 +4,7 @@
 int main(int /*argc*/, char** /*argv*/)
 {
   unsigned out {};
-  constexpr unsigned num_runs = 1e7;
+  constexpr unsigned num_runs = 10000000;
   for (unsigned run_id {} ; run_id != num_runs ; ++run_id)
   {
     out += most_significant_position(run_id);
