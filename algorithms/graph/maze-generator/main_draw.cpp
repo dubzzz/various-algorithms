63e1d3ad23b58907fd9b1d63a6792f7dbe5185da
Nicolas DUBIEN <github@dubien.org>
2017-01-27 05:06:07 +0100

    [graph/maze-generator] Add main_draw to display a maze of 80x80

---
+++ algorithms/graph/maze-generator/main_draw.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>
+
+#include "array2d.hpp"
+#include "maze.hpp"
+#include SPECIFIC_HEADER
+
+int main()
+{
+  auto dim = Dimension{80,80};
+  auto start_pt = Point{0,0};
+  auto end_pt = Point{79,79};
+
+  Array2D array2d(dim);
+  generate_maze(array2d.data(), dim, start_pt, end_pt, std::random_device{}());
+  array2d.print(std::cout);
+  
+  return array2d.data()[start_pt.y][start_pt.x] == to_char(MazeElement::Start)
+      && array2d.data()[end_pt.y][end_pt.x] == to_char(MazeElement::End);
+}
+
