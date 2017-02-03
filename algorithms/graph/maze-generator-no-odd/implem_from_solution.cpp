10e4cae786db7d455cefcc6bb78af00cac73884a
Nicolas DUBIEN <github@dubien.org>
2017-02-03 21:15:50 +0100

    [maze-generator-no-odd] Faster algorithm to find the solution

--- algorithms/graph/maze-generator-no-odd/implem_from_solution.cpp
+++ algorithms/graph/maze-generator-no-odd/implem_from_solution.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <stack>
 #include <vector>
 
 #include "aim.hpp"
@@ -12,13 +13,69 @@ struct Delta
   Delta(int x, int y) : x(x), y(y) {}
 };
 
+static bool is_inside(char** /*maze*/, Dimension const& dim, Point const& pt)
+{
+  return pt.x < dim.width && pt.y < dim.height;
+}
+static bool is_wall(char** maze, Dimension const& /*dim*/, Point const& pt)
+{
+  return maze[pt.y][pt.x] == to_char(MazeElement::Wall);
+}
+static bool is_inside_wall(char** maze, Dimension const& dim, Point const& pt)
+{
+  return is_inside(maze, dim, pt) && is_wall(maze, dim, pt);
+}
+
+struct UndoCanReach
+{
+  char** maze_;
+  Dimension dim_;
+  UndoCanReach(char** maze, Dimension const& dim) : maze_(maze), dim_(dim) {}
+  ~UndoCanReach()
+  {
+    const char visited { to_char(MazeElement::Undefined) };
+    const char wall { to_char(MazeElement::Wall) };
+    for (std::size_t j {} ; j < dim_.height ; j += 2)
+    {
+      for (std::size_t i {} ; i < dim_.width ; i += 2)
+      {
+        if (maze_[j][i] == visited) { maze_[j][i] = wall; }
+      }
+    }
+  }
+};
+static bool can_reach_end(char** maze, Dimension const& dim, Point const& origin, Point const& end)
+{
+  const char visited { to_char(MazeElement::Undefined) };
+  const char wall { to_char(MazeElement::Wall) };
+  UndoCanReach undoMe { maze, dim };
+
+  std::stack<Point> can_be_visited;
+  can_be_visited.push(origin);
+  while (! can_be_visited.empty())
+  {
+    Point pt { can_be_visited.top() };
+    can_be_visited.pop();
+
+    if (pt == end)
+    {
+      return true;
+    }
+    maze[pt.y][pt.x] = visited;
+    if (is_inside_wall(maze, dim, Point{pt.x-2, pt.y})) { can_be_visited.emplace(pt.x-2, pt.y); }
+    if (is_inside_wall(maze, dim, Point{pt.x+2, pt.y})) { can_be_visited.emplace(pt.x+2, pt.y); }
+    if (is_inside_wall(maze, dim, Point{pt.x, pt.y-2})) { can_be_visited.emplace(pt.x, pt.y-2); }
+    if (is_inside_wall(maze, dim, Point{pt.x, pt.y+2})) { can_be_visited.emplace(pt.x, pt.y+2); }
+  }
+  return false;
+}
+
 // Algorithm to be tested
 
 void generate_maze(char** maze, Dimension const& dim, Point const& start_pt, Point const& end_pt, unsigned seed)
 {
   auto const& width = dim.width;
   auto const& height = dim.height;
-  if (height * width >= 100) return;
   
   // Reset the map
   for (std::size_t j {} ; j != height ; ++j)
@@ -33,25 +90,21 @@ void generate_maze(char** maze, Dimension const& dim, Point const& start_pt, Poi
   std::mt19937 g(seed);
   std::vector<Point> toscan;
   
-  auto is_inside = [width,height](char** /*maze*/, Point const& pt) { return pt.x < width && pt.y < height; };
-  auto is_wall   = [](char** maze, Point const& pt) { return maze[pt.y][pt.x] == to_char(MazeElement::Wall); };
-  auto is_inside_wall = [&is_inside,&is_wall](char** maze, Point const& pt) { return is_inside(maze, pt) && is_wall(maze, pt); };
-  
-  auto push_if = [](auto& container, char** maze, Point const& pt, Delta const& delta, auto&& predicate) {
-      if (predicate(maze, Point{pt.x + 2*delta.x, pt.y + 2*delta.y})) { container.push_back(delta); }
+  auto push_if = [](auto& container, char** maze, Dimension const& dim, Point const& pt, Delta const& delta, auto&& predicate) {
+      if (predicate(maze, dim, Point{pt.x + 2*delta.x, pt.y + 2*delta.y})) { container.push_back(delta); }
   };
-  auto build_choices = [&push_if](char** maze, Point const& pt, auto&& predicate) {
+  auto build_choices = [&push_if](char** maze, Dimension const& dim, Point const& pt, auto&& predicate) {
     std::vector<Delta> choices;
-    push_if(choices, maze, pt, Delta{-1, 0}, predicate);
-    push_if(choices, maze, pt, Delta{ 1, 0}, predicate);
-    push_if(choices, maze, pt, Delta{ 0,-1}, predicate);
-    push_if(choices, maze, pt, Delta{ 0, 1}, predicate);
+    push_if(choices, maze, dim, pt, Delta{-1, 0}, predicate);
+    push_if(choices, maze, dim, pt, Delta{ 1, 0}, predicate);
+    push_if(choices, maze, dim, pt, Delta{ 0,-1}, predicate);
+    push_if(choices, maze, dim, pt, Delta{ 0, 1}, predicate);
     return choices;
   };
   
   std::vector<std::pair<Point, std::vector<Delta>>> path_to_end;
   auto current = Point{ start_pt };
-  auto choices = build_choices(maze, current, is_inside_wall);
+  auto choices = build_choices(maze, dim, current, is_inside_wall);
   while (current != end_pt)
   {
     if (choices.empty())
@@ -71,10 +124,17 @@ void generate_maze(char** maze, Dimension const& dim, Point const& start_pt, Poi
     std::size_t selected_idx { dist(g) };
     Delta d { choices[selected_idx] };
     choices.erase(choices.begin() + selected_idx);
+
+    // check if such path can lead to the end of the maze
+    maze[current.y][current.x] = to_char(MazeElement::Road);
+    if (! can_reach_end(maze, dim, Point{ current.x + 2 * d.x, current.y + 2 * d.y }, end_pt))
+    {
+      maze[current.y][current.x] = to_char(MazeElement::Wall);
+      continue;
+    }
     
     // current decision
     path_to_end.emplace_back(current, std::move(choices));
-    maze[current.y][current.x] = to_char(MazeElement::Road);
     
     // junction road
     current = Point{ current.x + d.x, current.y + d.y };
@@ -83,7 +143,7 @@ void generate_maze(char** maze, Dimension const& dim, Point const& start_pt, Poi
     
     // next position
     current = Point{ current.x + d.x, current.y + d.y };
-    choices = build_choices(maze, current, is_inside_wall);
+    choices = build_choices(maze, dim, current, is_inside_wall);
   }
   
   // Put start and end
