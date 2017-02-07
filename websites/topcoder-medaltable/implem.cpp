a049f6d960254be6975641719602025c102560e2
Nicolas DUBIEN <github@dubien.org>
2017-02-07 02:52:18 +0100

    [topcoder-medaltable] Remove the use of std::map
    Attempt to reduce the number of raw loops

--- websites/topcoder-medaltable/implem.cpp
+++ websites/topcoder-medaltable/implem.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <iterator>
-#include <map>
+#include <numeric>
 #include <sstream>
 #include <string>
 #include <tuple>
@@ -22,20 +22,19 @@ std::vector<std::string> medal_table(std::vector<std::string> const& results)
         , std::back_inserter(flat_results)
         , [](auto const& name, auto idx) { return std::make_pair(name, idx); });
   }
+  std::sort(std::begin(flat_results), std::end(flat_results), [](auto const& i1, auto const& i2) { return i1.first < i2.first; });
   
-  std::map<std::string, std::vector<unsigned>> data;
-  for (auto const& entry : flat_results)
-  {
-    auto current = data.find(entry.first);
-    if (current == data.end())
-    {
-      current = data.emplace(entry.first, std::vector<unsigned>{0, 0, 0}).first;
-    }
-    ++current->second[entry.second];
-  }
-  
-  std::vector<std::pair<std::string, std::vector<unsigned>>> ranking;
-  std::copy(std::begin(data), std::end(data), std::back_inserter(ranking));
+  auto ranking = std::accumulate(
+      std::begin(flat_results), std::end(flat_results)
+      , std::vector<std::pair<std::string, std::vector<unsigned>>>{}
+      , [](auto&& acc, auto entry) {
+          if (acc.empty() || acc.back().first != entry.first)
+          {
+            acc.emplace_back(entry.first, std::vector<unsigned>{0, 0, 0});
+          }
+          ++acc.back().second[entry.second];
+          return acc;
+      });
   std::sort(std::begin(ranking), std::end(ranking)
       , [](auto const& p1, auto const& p2) {
           return !(p1.second < p2.second || (p1.second == p2.second && p1.first > p2.first));
