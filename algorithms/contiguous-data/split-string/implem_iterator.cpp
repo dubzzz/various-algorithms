8056ae71af00d7249982658536c512dff70ccb5c
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-03-08 17:44:53 +0100

    Update implem_iterator.cpp

--- algorithms/contiguous-data/split-string/implem_iterator.cpp
+++ algorithms/contiguous-data/split-string/implem_iterator.cpp
@@ -11,17 +11,15 @@
 
 namespace {
 
-struct Splitter
+struct Splitter : std::string
 {//not safe for multi-threading
   static char delim; //crappy just to test with stream iterators
-  std::string m_s;
-  Splitter() : m_s() {}
 };
 char Splitter::delim = ' ';
 
 std::istream& operator>>(std::istream& in, Splitter& sp)
 {
-  std::getline(in, sp.m_s, Splitter::delim);
+  std::getline(in, sp, Splitter::delim);
   return in;
 }
 
@@ -31,13 +29,9 @@ std::vector<std::string> split(std::string const& in, char delim)
 {
   Splitter::delim = delim;
   
-  std::vector<Splitter> tmp;
+  std::vector<std::string> out;
   std::istringstream iss(in);
-  std::copy(std::istream_iterator<Splitter>(iss), std::istream_iterator<Splitter>(), std::back_inserter(tmp));
-  for (std::size_t pos {in.size()} ; pos > 0 && in[pos-1] == delim ; --pos) { tmp.emplace_back(); }
-  
-  std::vector<std::string> out(tmp.size());
-  std::transform(tmp.begin(), tmp.end(), out.begin(), [](auto& v) { return std::move(v.m_s); });
+  std::copy(std::istream_iterator<Splitter>(iss), std::istream_iterator<Splitter>(), std::back_inserter(out));
+  for (std::size_t pos {in.size()} ; pos > 0 && in[pos-1] == delim ; --pos) { out.emplace_back(); }
   return out;
 }
-
