43f1e32ff97374db6cbd77d42fdebafe0dcae88b
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-03-14 09:06:38 +0100

    [maximal-rectangle] Try fix

--- algorithms/contiguous-data/maximal-rectangle/implem.cpp
+++ algorithms/contiguous-data/maximal-rectangle/implem.cpp
@@ -15,22 +15,22 @@ std::size_t largest_area(std::vector<std::vector<bool>> const& matrix)
     //   for all j such as last_start[i] <= j < idx
     //     matrix[j][i] is true
     
-    // compute bars
-    for (std::size_t i {} ; i != last_start.size() ; ++i)
-    {
-      if (last_start[i] >= idx) { continue; }
-      std::size_t starts_at = i;
-      std::size_t ends_at = i;
-      while (starts_at > 0 && last_start[starts_at-1] <= last_start[i]) { --starts_at; }
-      while (ends_at+1 < last_start.size() && last_start[ends_at+1] <= last_start[i]) { ++ends_at; }
-      max_area = std::max(max_area, (ends_at-starts_at+1)*(idx-last_start[i]));
-    }
-    
     // update starts
     for (std::size_t i {} ; i != last_start.size() ; ++i)
     {
       if (! matrix[idx][i]) { last_start[i] = idx +1; }
       else if (last_start[i] >= idx) { last_start[i] = idx; }
+    }
+  
+    // compute bars
+    for (std::size_t i {} ; i != last_start.size() ; ++i)
+    {
+      if (last_start[i] > idx) { continue; }
+      std::size_t starts_at = i;
+      std::size_t ends_at = i;
+      while (starts_at > 0 && last_start[starts_at-1] <= last_start[i]) { --starts_at; }
+      while (ends_at+1 < last_start.size() && last_start[ends_at+1] <= last_start[i]) { ++ends_at; }
+      max_area = std::max(max_area, (ends_at-starts_at+1)*(idx-last_start[i]+1));
     }
   }
   return max_area;
